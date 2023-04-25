66668d056e5a5036fc776c0d561624d47e35d855
Adeola Tawakalitu Adenike <tawa@Adeolas-MacBook-Pro.local>
2023-04-25 13:47:44 +0100

    image path

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,6 +1,6 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
-import JobList from '../assets/data.json'
+import JobList from '@/data.json'
 
 export const useJobStore = defineStore('JobList', {
   state: () => {
